415d7b2ddf4ee27abaa8a22fee5a44e367ed9b62
40204509 <40204509@hruc.ac.uk>
2024-12-10 16:50:19 +0000

    Titles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 export default function Header() {
     return (
-        <div className="bg-[#272D40] text-[#F0F0F0] text-[40px] h-[114px] flex justify-center items-center">
-            Amazing header
+        <div className="bg-[#272D40] text-[#F0F0F0] text-[40px] h-[114px] flex justify-center items-center w-full gap-36">
+            <div>INFORMATION</div>
+            <div>WEATHER</div>
+            <div>AIR QUALITY</div>
         </div>
     )
 }
