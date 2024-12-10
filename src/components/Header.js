c86e8e94cc7075a7d5ae66c152b6c20bfb7f0710
40204509 <40204509@hruc.ac.uk>
2024-12-10 16:40:00 +0000

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="bg-red-400">
+        <div className="bg-[#272D40] text-[#F0F0F0] text-[40px] h-[114px] flex justify-center items-center">
             Amazing header
         </div>
     )
